ed6e9dc2ec95f7d2904eab43e925e87c66ed2a92
saalcazar <alcazar.samuel@gmail.com>
2023-03-14 17:04:58 -0400

    Corección de errores

--- model/model.go
+++ model/model.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	//ErrPersonCanNotBeNil
 	ErrPersonCanNotBeNil     = errors.New("la persona no puede ser nula")
-	ErrIDPersonDoesNotExists = errors.New("La persona no existe ")
+	ErrIDPersonDoesNotExists = errors.New("la persona no existe ")
 )
